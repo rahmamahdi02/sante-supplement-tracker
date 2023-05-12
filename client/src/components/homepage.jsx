530ff85d844bee15a06af2dbf8624a1aba2eed81
TPL1122_18 <tpl1122_18@TPL1122-18s-MacBook-Pro.local>
2023-05-12 05:14:47 -0700

    import components from Semntic UI library

---
+++ client/src/components/homepage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {
+    Button,
+    Container,
+    Divider,
+    Grid,
+    Header,
+    Icon,
+    Image,
+    List,
+    Menu,
+    Segment,
+    Sidebar,
+  } from 'semantic-ui-react'
+
+
+  
