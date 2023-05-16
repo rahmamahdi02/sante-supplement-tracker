6d1a7b5cc508c7388974de8617bc66a4b481489d
TPL1122_18 <tpl1122_18@TPL1122-18s-MacBook-Pro.local>
2023-05-16 08:03:59 -0700

    add  new changes

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,37 +1,37 @@
 import React from 'react';
-import { Container, Grid, Image, Header } from 'semantic-ui-react';
-import HeroImage from '../assets/hero_image.png'
+import { Container, Header } from 'semantic-ui-react';
+import Logo from '../assets/santelogo.png'
 
-const Hero = () => {
+
+const HeroSection = () => {
+
+  const heroStyle = {
+    src={Logo}
+    backgroundize: 'cover',
+    backgroundPosition: 'center',
+    height: '740px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  };
+
   return (
-    <div className="hero-section">
-      <Container>
-        <Grid>
-          <Grid.Row>
-            <Grid.Column mobile={16} computer={8}>
-              <Header as="h1">Welcome to the Hero Section</Header>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Phasellus nec gravida nisi, vitae varius elit. Nulla eu
-                scelerisque lacus, vitae efficitur velit. Curabitur
-                sollicitudin justo vel justo efficitur, at pharetra turpis
-                luctus.
-              </p>
-              <button className="ui primary button">Get Started</button>
-            </Grid.Column>
-            <Grid.Column mobile={16} computer={8}>
-              <Image
-                src={HeroImage}
-                alt="Hero Image"
-                size="large"
-                rounded
-              />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
+    <div className="hero-section" style={heroStyle}>
+      <Container text>
+        <Header as="h1" inverted>
+          Welcome to the Hero Section
+        </Header>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Phasellus nec gravida nisi, vitae varius elit. Nulla eu
+          scelerisque lacus, vitae efficitur velit. Curabitur
+          sollicitudin justo vel justo efficitur, at pharetra turpis
+          luctus.
+        </p>
+        <button className="ui primary button">Get Started</button>
       </Container>
     </div>
   );
 };
 
-export default Hero;
+export default HeroSection;
