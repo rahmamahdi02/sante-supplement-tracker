af39419f5644451176fa23cfcb132c432450565d
TPL1122_18 <tpl1122_18@TPL1122-18s-MacBook-Pro.local>
2023-05-16 10:24:56 -0700

    saving changes after code editor accidently shut down

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,37 +1,36 @@
 import React from 'react';
-import { Container, Header } from 'semantic-ui-react';
-import Logo from '../assets/santelogo.png'
+import { Container, Grid, Image, Header } from 'semantic-ui-react';
 
-
-const HeroSection = () => {
-
-  const heroStyle = {
-    src={Logo}
-    backgroundize: 'cover',
-    backgroundPosition: 'center',
-    height: '740px',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  };
-
+const Hero = () => {
   return (
-    <div className="hero-section" style={heroStyle}>
-      <Container text>
-        <Header as="h1" inverted>
-          Welcome to the Hero Section
-        </Header>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Phasellus nec gravida nisi, vitae varius elit. Nulla eu
-          scelerisque lacus, vitae efficitur velit. Curabitur
-          sollicitudin justo vel justo efficitur, at pharetra turpis
-          luctus.
-        </p>
-        <button className="ui primary button">Get Started</button>
+    <div className="hero-section">
+      <Container style={{ padding: '8em 0em' }} >
+        <Grid>
+          <Grid.Row>
+            <Grid.Column mobile={16} computer={8}>
+              <Header as="h1">Welcome to the Hero Section</Header>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Phasellus nec gravida nisi, vitae varius elit. Nulla eu
+                scelerisque lacus, vitae efficitur velit. Curabitur
+                sollicitudin justo vel justo efficitur, at pharetra turpis
+                luctus.
+              </p>
+              <button className="ui primary button">Get Started</button>
+            </Grid.Column>
+            <Grid.Column mobile={16} computer={8}>
+              <Image
+                src="/path/to/your/image.jpg"
+                alt="Hero Image"
+                size="large"
+                rounded
+              />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </Container>
     </div>
   );
 };
 
-export default HeroSection;
+export default Hero;
