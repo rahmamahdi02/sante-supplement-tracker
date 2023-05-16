60145a6b00cbbc24d160585ebde013281ffdac5f
TPL1122_18 <tpl1122_18@TPL1122-18s-MacBook-Pro.local>
2023-05-15 21:22:04 -0700

    debug syntax error in components folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import MyNavBar from './components/Navbar'
-import ListStudents from './components/ListStudents'
-import HomepageLayout from './components/HomepageLayout'
+import MyNavBar from './components/Navbar';
+import ListStudents from './components/ListStudents';
+import HomepageLayout from './components/HomepageLayout';
 import Hero from  './components/Hero';
-import { Button } from 'semantic-ui-react'
+import About from './components/about';
+import Footer from './components/footer';
 
+// import Footer from './components/footer';
+
+import { Button } from 'semantic-ui-react';
 
 function App() {
 
@@ -14,13 +18,16 @@ function App() {
       <MyNavBar />
 
       <Hero />
-      
+
+      < About />
+
+{/* < Footer /> */}
+
       <ListStudents />
 
       
 
       <Button color = "red" >Standard</Button>
-
  {/* <HomepageLayout/> */}
 
     </div>
