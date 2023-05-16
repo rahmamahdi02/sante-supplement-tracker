8ba354130e49f3c05d5791ef02bc2bdb06cf0162
TPL1122_18 <tpl1122_18@TPL1122-18s-MacBook-Pro.local>
2023-05-16 05:41:50 -0700

    update all changes for components

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -16,7 +16,7 @@ import {
 const footer = () => {
   return (
     <div >
-          <Segment inverted vertical style={{ background: 'orange', padding: '8em 0em' }}>
+          <Segment inverted vertical style={{ background: 'dark', padding: '8em 0em' }}>
           <Container>
 
           <Grid divided inverted stackable>
