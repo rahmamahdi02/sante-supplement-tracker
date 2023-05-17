fce3056c004a7fa3c6d5de845b04335bf1b564ec
TPL1122_18 <tpl1122_18@TPL1122-18s-MacBook-Pro.local>
2023-05-17 15:27:18 -0700

    update readme doc

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,12 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import MyNavBar from './components/Navbar'
 import ListStudents from './components/ListStudents'
+import HomepageLayout from './components/HomepageLayout'
+// import Homepage from './components/Homepage'
 import Hero from  './components/Hero'
 import About from './components/about'
 import Footer from './components/footer';
-import Tracker from  './components/tracker';
-import Calculator from './components/calculator';
-
-// import Homepage from './components/Homepage'
-// import HomepageLayout from './components/HomepageLayout'
-// import { Button } from 'semantic-ui-react'
+import { Button } from 'semantic-ui-react'
 
 function App() {
 
@@ -26,13 +23,7 @@ function App() {
 
            < About />
 
-           <Calculator />
-
-           <Tracker />
-
-
         <ListStudents />
-        
       
       <Footer />
 
