1b2026c65b595685a1d874e5b2e2212518babfc5
TPL1122_18 <tpl1122_18@TPL1122-18s-MacBook-Pro.local>
2023-05-17 14:43:39 -0700

    redordered components in app.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,15 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import MyNavBar from './components/Navbar'
 import ListStudents from './components/ListStudents'
-import HomepageLayout from './components/HomepageLayout'
-// import Homepage from './components/Homepage'
 import Hero from  './components/Hero'
 import About from './components/about'
 import Footer from './components/footer';
+import Tracker from  './components/tracker';
 import Calculator from './components/calculator';
-import { Button } from 'semantic-ui-react'
+
+// import Homepage from './components/Homepage'
+// import HomepageLayout from './components/HomepageLayout'
+// import { Button } from 'semantic-ui-react'
 
 function App() {
 
@@ -23,8 +25,10 @@ function App() {
 
 
            < About />
-           
+
            <Calculator />
+
+           <Tracker />
 
 
         <ListStudents />
