419e8e0d80728c5f5a03dacf2c3e03ffdf573743
Rahma <you@example.com>
2023-05-20 23:45:07 -0700

    rewrite a prior version of main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,25 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import 'semantic-ui-css/semantic.min.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+      
     <App />
+
+    <RouterProvider router={router} />
+    
   </React.StrictMode>,
 )
