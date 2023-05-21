c99011b7de052a6e9639ca9d8646307b0380bc33
Rahma <you@example.com>
2023-05-21 06:57:39 -0700

    mock api call using axios

---
+++ client/src/components/vitamincard.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const FactsheetComponent = () => {
+  const [factsheets, setFactsheets] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://api.ods.od.nih.gov/dsld/v9/ingredient-groups/', {
+          params: {
+            method: 'factsheet',
+            term: 'Folic Acid'
+          }
+        });
+        setFactsheets(response.data.results);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {factsheets.map(factsheet => (
+        <div key={factsheet.id} className="card">
+          <div className="card-body">
+            <h5 className="card-title">{factsheet.title}</h5>
+            <p className="card-text">{factsheet.url}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default FactsheetComponent;
