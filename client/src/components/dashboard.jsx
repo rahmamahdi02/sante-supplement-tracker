d6530f92a1648d865265ce87c820c66a2ef9e2d0
Rahma <rahmamahdi.02@gmail.com>
2023-05-22 03:36:45 -0700

    refactored components

--- client/src/components/dashboard.jsx
+++ client/src/components/dashboard.jsx
@@ -1,36 +1,25 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import MyNavBar from './components/Navbar'
 import ListStudents from './components/ListStudents'
-import HomepageLayout from './components/HomepageLayout'
-import Hero from  './components/Hero'
-import About from './components/about'
 import Tracker from  './components/tracker'
 import Calculator from  './components/calculator'
-import Footer from './components/footer';
 import MedicationTable from './components/medicationtracker'
-import SideProfile from './components/profile'
-import Banner from './components/banner'
+
 import FactsheetComponent from './components/vitamincard'
-function App() {
+
+function Dashboard() {
 
   return (
-    <div className="App">
-
-      <MyNavBar />
-         <Hero />
-           < About />
-           < SideProfile />
-           < Banner />
-< Calculator />
-< Tracker /> 
-< MedicationTable />
-<FactsheetComponent />
-        <ListStudents />    
-      <Footer />
+    <div className="Dashboard-Styling">
+       < Calculator />
+       < Tracker /> 
+       < MedicationTable />
+       <FactsheetComponent />
+       <ListStudents />    
+     
 
     </div>
   )
 }
 
-export default App
+export default Dashboard
