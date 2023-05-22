bb5d8b168bdb92938b98c6c5ad21c59e84fce7c1
Rahma <rahmamahdi.02@gmail.com>
2023-05-22 08:36:51 -0700

    import routes into app.js file

--- client/src/components/homepage.jsx
+++ client/src/components/homepage.jsx
@@ -1,19 +1,34 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Hero from  './components/Hero'
-import About from './components/about'
-import Banner from './components/banner'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-function Homepage() {
+import About from "./pages/About.jsx";
+import Home from "./pages/Home.jsx";
+import Profile from "./pages/Profile.jsx";
+import Root from "./pages/Root.jsx";
+import 'bootstrap/dist/css/bootstrap.css';
 
-  return (
-    <div className="Homepage-Styling">
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        children: [
+            {
+                index: true,
+                element: <Home />,
+            },
+            {
+                path: 'about',
+                element: <About />,
+            },
+            {
+                path: 'profile',
+                element: <Profile />,
+            }
+        ],
+    },
+]);
 
-         <Hero />
-           < About />
-           < Banner />   
+const App = () => (
+    <RouterProvider router={router}/>
+);
 
-    </div>
-  )
-}
-
-export default Homepage
+export default App;
