7d51fc69282ee83a3b3acae6c9048dd161420b76
Rahma <rahmamahdi.02@gmail.com>
2023-05-22 09:18:40 -0700

    finalized setup for router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,28 @@
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import MyNavBar from "./components/Navbar";
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MyNavBar  from '../src/routes/Navbar'
+import Home from './routes/Home';
+import ErrorPage from './components/error-page'
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import Profile from './components/Profile';
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Profile from "./pages/Profile";
-import Family from "./pages/Family";
-import Friends from "./pages/Friends";
-import Root from "./pages/Root";
-import Home from "./pages/Home";
 
-const router = createBrowserRouter([
-  {
-    children: [
-      { index: true, element: <Home /> },
-      {
-        path: "/profile",
-        element: <Profile />,
-      },
-      {
-        path: "/family",
-        element: <Family />,
-      },
-      {
-        path: "/friends",
-        element: <Friends />,
-      },
-    ],
-    path: "/",
-    element: <Root />,
-  },
-]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MyNavBar />}>
+      <Route index element={<Home />} errorElement={<ErrorPage />} />
+      <Route path="user-profile" element={<Profile/>} />
+    </Route>
+  )
+)
+
+
 function App() {
-  return <RouterProvider router={router} />;
+
+  return (
+    
+    <RouterProvider router={router} />
+  )
 }
 
-export default App;
+export default App
