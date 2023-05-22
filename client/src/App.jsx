fd3c1e48f55fa68afc698ef4d8fc5270c1be68ac
Rahma <rahmamahdi.02@gmail.com>
2023-05-22 03:46:48 -0700

    routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,23 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import MyNavBar from './components/Navbar'
+import Homepage from './components/homepage';
+// import Dashboard from './components/dashboard';
 import ListStudents from './components/ListStudents'
-import HomepageLayout from './components/HomepageLayout'
-import Hero from  './components/Hero'
-import About from './components/about'
-import Tracker from  './components/tracker'
-import Calculator from  './components/calculator'
+
 import Footer from './components/footer';
-import MedicationTable from './components/medicationtracker'
-import SideProfile from './components/profile'
-import Banner from './components/banner'
-import FactsheetComponent from './components/vitamincard'
+
+
+
 function App() {
 
   return (
     <div className="App">
 
       <MyNavBar />
-         <Hero />
-           < About />
-           < SideProfile />
-           < Banner />
-< Calculator />
-< Tracker /> 
-< MedicationTable />
-<FactsheetComponent />
-        <ListStudents />    
+<Homepage />
+{/* <Dashboard />  */}
+<ListStudents />
       <Footer />
 
     </div>
